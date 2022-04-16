3ccb40822b7f73e545007dc790b7b1788260442e
Fleexii <103858656+Fleexii@users.noreply.github.com>
2022-04-17 00:12:06 +0300

    Add files via upload

---
+++ src/main/java/net/Fleexii/hudmod/DiamondCountMod.java
@@ -0,0 +1,13 @@
+package net.Fleexii.hudmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiamondCountMod implements ClientModInitializer {
+
+    public static final String MOD_ID = "inventory_hud";
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
